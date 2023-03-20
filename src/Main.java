ddae6bde110ead81b77bd66c13b703a1eacedca2
Sheldon <aneln700@gmail.com>
2023-03-20 17:55:33 +0300

    Class

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
-public class Main {
-    public static void main(String[] args) {
-
-        int price = 20000;
-        int mile = 20;
-        int bonus = price / mile;
-        System.out.println("Начислено бонусов:" + bonus);
+ public class Main {
+        public static void main(String[] args) {
+            BonusMilesService service = new BonusMilesService();
+            int price = 10_000;
+            int miles = service.calculate(price); // должно получиться 500
+            System.out.println(miles);
+        }
     }
-}
